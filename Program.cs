d626de80fb24afe4b218ff71bdbf8af6b3894be7
ADMIN$ <ADMIN$@DESKTOP-9TIC5VQ>
2023-02-25 19:01:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Globalization;
+
+public class program
+{
+
+	public static void Main()
+	{
+		//PrintNumber();
+		NestedLoop();
+	}
+
+	public static void PrintNumber()
+	{
+		// 1. Print Number 1 To 10
+		
+		
+		        //Initialization  //Condition      //Inc/Dec
+		for(int        i=1;          i <=10;           i++)
+		{
+			Console.Write(" "+i);
+		}
+
+		// 2.Print Number 5,10,15,.....,50
+
+		for(int i=5; i <= 50; i = i + 5)
+		{
+			Console.Write(" "+i);
+		}
+
+		// 3. Print Reverse Number Between 20 To 1
+
+		for (int i = 20; i >= 1; i--)
+		{
+			Console.Write(" "+i);
+		}
+
+		// 4.Print Number 6,12,18,......60
+		for(int i = 6; i <= 60; i = i + 6)
+		{
+			Console.Write(" "+i);
+		}
+
+		//5. Print Reverse Number 10,8,6,....,1
+		for(int i = 10; i >= 0; i = i - 2)
+		{
+			Console.Write(" "+i);
+		}
+	}
+
+	public static void NestedLoop()
+	{
+		// Print Number 54321
+		for (int i = 1; i <= 4; i++) //Rows
+		{
+			for (int j = 5; j >= 1; j--) //Columns
+			{
+				Console.Write(j);
+			}
+			Console.WriteLine();
+		}
+
+		// Print Number 12345 
+
+		for(int m = 1; m <= 5; m++)
+		{
+			for(int n = 1; n <= 5; n++)
+			{
+				Console.Write(n);
+			}
+			Console.WriteLine();
+		}
+
+		// Print Number 11111,22222,....55555
+
+		for(int l = 1; l <= 5; l++)
+		{
+			for(int m = 1; m <= 5; m++)
+			{
+				Console.Write(l);
+			}
+			Console.WriteLine();
+		}
+
+		// Print Number 55555,44444,....,11111
+
+		for(int a = 5; a >= 1; a--)
+		{
+			for(int b = 1; b <= 5; b++)
+			{
+				Console.Write(a);
+			}
+			Console.WriteLine();
+		}
+	}
+
+}
